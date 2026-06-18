af50bc8468746d965f16c777f371e04edcc3cc97
Neeraj Singh <neerajsingh@Neerajs-MacBook-Pro-2.local>
2026-06-18 20:04:07 +0530

    add new Project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -2,6 +2,12 @@ import { RevealOnScroll } from "../RevealOnScroll";
 import { TiltCard } from "../TiltCard";
 import { motion } from "framer-motion";
 import { ExternalLink, Github } from "lucide-react";
+import futureWorkImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/future-work.png";
+import netflixImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/netflix.png";
+import zomatoImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/zomato.png";
+import musicPlayerImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/harmonix.png";
+import fitnessAppImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/wellora.png";
+import exoplanetImg from "/Users/neerajsingh/Desktop/all folder/portfolio/my-portfolio/src/assets/exoplanet.png";
 
 const projects = [
   {
@@ -9,6 +15,7 @@ const projects = [
     description: "A dynamic music player application with features like play/pause, next/previous track, progress bar, playlist management, and favourites/liked songs. Integrated Firebase for user authentication and secure data handling.",
     tech: ["React", "Context API", "Tailwind CSS", "Firebase"],
     link: "https://harmonix-psi.vercel.app/",
+    image: musicPlayerImg,
     color: "blue"
   },
   {
@@ -16,6 +23,7 @@ const projects = [
     description: "Engineered a full-stack fitness application to help users track workouts, build healthy habits, and maintain consistency. Developed a responsive, intuitive UI using React and implemented robust backend services.",
     tech: ["React", "Tailwind", "Express", "MongoDB", "Node.js"],
     link: "https://wellora-pi.vercel.app/",
+    image: fitnessAppImg,
     color: "cyan"
   },
   // {
@@ -33,17 +41,28 @@ const projects = [
   //   color: "purple"
   // },
   {
-    title: "Zomato Delivery & Business Analytics Dashboard",
-    description: "Analyzed 45K+ delivery records to uncover operational and business trends, built interactive Tableau dashboards with dynamic filters (city, weather, traffic, order type), tracked key KPIs including total orders, average delivery time (~26 minutes), and customer rating (4.63), identified that over 77% of demand comes from metro cities with clear peak ordering patterns, examined the impact of traffic and weather on delays (rising to ~31 minutes in high congestion), highlighted inefficiencies such as multi-order deliveries increasing time (~47 minutes), and designed a clean, navigable UI covering business, delivery, and partner insights.",
-    tech: ["Tableau", "Excel"],
-    link: "https://public.tableau.com/app/profile/neeraj.singh1868/viz/Capstone2Dashboard_17769517459070/Dashboard4?publish=yes",
-    color: "green"
+    title:"Future of Work 2030: AI, Jobs, Salaries & Skills Analytics Dashboard",
+    description:"Designed a multi-dashboard Tableau project analyzing the impact of AI adoption on industries, job growth, automation risk, salary trends, and future workforce skills. Conducted data-driven research to identify high-growth sectors, automation-vulnerable industries, highest-paying skills, and workforce recommendations through interactive visualizations and KPI-driven insights.",
+    tech: ["Tableau", "Excel", "Data Analysis", "Data Visualization", "Dashboard Design", "Calculated Fields", "KPI Analytics"],
+    link: "https://public.tableau.com/app/profile/neeraj.singh1868/viz/TheFutureofWork2030AIJobsSalariesSkills/Dashboard?publish=yes",
+    image: futureWorkImg,
+    color: "blue"
+
   },
   {
     title: "Netflix Content Distribution Analytics",
     description: "Analyzed 8,778 titles to identify content distribution patterns (69.6% movies, 30.4% TV shows) across genre,rating, and temporal dimensions. Conducted temporal trend analysis revealing rapid content growth post-2015, with 2018 as the peak release year — informing content strategy insights",
     tech: ["Tableau", "Excel"],
     link: "https://public.tableau.com/app/profile/neeraj.singh1868/viz/Netflix_17779544198340/Dashboard?publish=yes",
+    image: netflixImg,
+    color: "green"
+  },
+  {
+    title: "Zomato Delivery & Business Analytics Dashboard",
+    description: "Analyzed 45K+ delivery records to uncover operational and business trends, built interactive Tableau dashboards with dynamic filters (city, weather, traffic, order type), tracked key KPIs including total orders, average delivery time (~26 minutes), and customer rating (4.63), identified that over 77% of demand comes from metro cities with clear peak ordering patterns, examined the impact of traffic and weather on delays (rising to ~31 minutes in high congestion), highlighted inefficiencies such as multi-order deliveries increasing time (~47 minutes), and designed a clean, navigable UI covering business, delivery, and partner insights.",
+    tech: ["Tableau", "Excel"],
+    link: "https://public.tableau.com/app/profile/neeraj.singh1868/viz/Capstone2Dashboard_17769517459070/Dashboard4?publish=yes",
+    image: zomatoImg,
     color: "green"
   },
     {
@@ -51,6 +70,7 @@ const projects = [
     description: "Analyzed 5,000+ NASA Exoplanet Archive records across seven detection methods, identifying observational bias (including hot-Jupiter overrepresentation in radial velocity data) and confirming that the Transit method has driven 70%+ of discoveries since 2009, consistent with Kepler Mission benchmarks. Cleaned and standardized large-scale astronomical data by handling missing values and unit inconsistencies, and built an interactive Looker Studio dashboard to visualize discovery trends, mission timelines, and planetary characteristics.",
     tech: ["Tableau", "Excel"],
     link: "https://github.com/Neeraj-singh140805/SectionC_Group09_ExoPlanets",
+    image: exoplanetImg,
     color: "green"
   }
 ];
@@ -106,9 +126,14 @@ export const Projects = () => {
                     <div className={`absolute -bottom-24 -left-24 w-64 h-64 ${glowMap[project.color]} rounded-full blur-[80px] -z-10 group-hover:scale-150 transition-transform duration-700`}></div>
                     
                     <div className="flex justify-between items-start mb-6">
-                      <h3 className={`text-3xl font-bold text-white group-hover:${textMap[project.color]} transition-colors duration-300`}>
+                      {/* <h3 className={`text-3xl font-bold text-white group-hover:${textMap[project.color]} transition-colors duration-300`}>
                         {project.title}
-                      </h3>
+                      </h3> */}
+                      <img
+                      src={project.image}
+                      alt={project.title}
+                      className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-110"
+                    />
                       <a 
                         href={project.link}
                         target="_blank"
